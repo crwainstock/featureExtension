8b1c01eab6199bf1d58db4927796eb2840a2a058
Crystal <crwainstock@gmail.com>
2023-03-06 15:55:13 +0100

    cleaning up app.js file and getting things started

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 //HOMEPIC LINK: https://qph.cf2.quoracdn.net/main-qimg-8cff84922129f22abfa62cc8636d5901.webp
 
 function Home(props) {
-	return (
-		<>
-			<div className="backgroundDiv bg-image">
-				<h1 className="display-1 fw-bold">AniColors</h1>
+  return (
+    <>
+      <div className="backgroundDiv bg-image">
+        <h1 className="display-1 fw-bold">AniColors</h1>
 
-				<p className="lead">
-					There are endless words on Earth, as many as we can see.
-					<br />
-					Would you like to see the world through other animals' eyes?
-				</p>
+        <p className="lead">
+          There are endless worlds on Earth, as many as we can see.
+          <br />
+          Would you like to see the world through other animals' eyes?
+        </p>
 
-				<div className="buttonDiv">
-					<NavLink to="/action">
-						<Button className="actionBtn" variant="outline-light" size="lg">
-							TAKE A LOOK{' '}
-						</Button>
-					</NavLink>
-				</div>
-			</div>
-		</>
-	);
+        <div className="buttonDiv">
+          <NavLink to="/action">
+            <Button className="actionBtn" variant="outline-light" size="lg">
+              TAKE A LOOK{" "}
+            </Button>
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;

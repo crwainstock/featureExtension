55f231f95f5ba82a9ed278b2694b3223b517eb63
Crystal <crwainstock@gmail.com>
2023-03-07 09:23:00 +0100

    added active-link class to css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,30 +4,24 @@ import { NavLink } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 
 export default function Header() {
-  const activeStyle = {
-    "font-weight": "bold",
-    "text-decoration": "underline",
-    color: "#161616",
-  };
-
   return (
     <header>
       <Nav>
         <NavLink
           to="/"
-          className={({ isActive }) => (isActive ? activeStyle : null)}
+          className={({ isActive }) => (isActive ? "active-link" : null)}
         >
           Home
         </NavLink>
         <NavLink
           to="/look"
-          className={({ isActive }) => (isActive ? activeStyle : null)}
+          className={({ isActive }) => (isActive ? "active-link" : null)}
         >
           LOOK
         </NavLink>
         <NavLink
           to="/about"
-          className={({ isActive }) => (isActive ? activeStyle : null)}
+          className={({ isActive }) => (isActive ? "active-link" : null)}
         >
           About
         </NavLink>

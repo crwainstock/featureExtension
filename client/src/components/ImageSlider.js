e9611a3af3aebce2c0237bb218c5ac654645f0ba
Crystal <crwainstock@gmail.com>
2023-03-07 19:42:46 +0100

    trying to build image slider

--- client/src/components/ImageSlider.js
+++ client/src/components/ImageSlider.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "../components/imageSlider.css";
 
 export default function ImageSlider() {
   return (
     <div className="container">
       <h3>Image slider goes here</h3>
+      <div className="img-comp-container">
+        <div className="img-comp-img">
+          <img src="./images/animalVision/human.png" width="300" height="200" />
+        </div>
+        <div className="img-comp-img img-comp-overlay">
+          <img src="./images/animalVision/bee.png" width="300" height="200" />
+        </div>
+      </div>
     </div>
   );
 }

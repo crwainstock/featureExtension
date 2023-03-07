9c6bef3c6ee5bffa3432f2cc715706ed022c6e90
Crystal <crwainstock@gmail.com>
2023-03-07 13:34:52 +0100

    get two columns into one for mobile

--- client/src/pages/Action.js
+++ client/src/pages/Action.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { Routes, Route, NavLink, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 import "./action.css";
 
 import Container from "react-bootstrap/Container";
@@ -7,77 +6,26 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-import Figure from "react-bootstrap/Figure";
-import FigureImage from "react-bootstrap/FigureImage";
-import FigureCaption from "react-bootstrap/FigureCaption";
 
 import defaultImg from "../images/spider_eyes.jpg";
 import catImage from "../images/icons/cat.png";
 import cowImage from "../images/icons/cow.png";
 import whaleImage from "../images/icons/whale.png";
 import beeImage from "../images/icons/bee.png";
-import pixabayLogo from "../images/logo_square.png";
 
-const url = `https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&category=nature;animals&image_type=photo`;
-
-export default function Action(props) {
+export default function Action() {
   const [animal, setAnimal] = useState(null);
   const [error, setError] = useState("");
   const [image, setImage] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [filterClass, setFilterClass] = useState(null);
-  // const [checked, setChecked] = useState(null);
-
-  const handleFilter = (e) => {
-    setAnimal(e.target.value);
-    changeColor(e.target.value);
-  };
-
-  const changeColor = (animal) => {
-    if (animal === "cat") {
-      setFilterClass("catFilter");
-    }
-    if (animal === "cow") {
-      setFilterClass("cowFilter");
-    }
-    if (animal === "whale") {
-      setFilterClass("whaleFilter");
-    }
-    if (animal === "bee") {
-      setFilterClass("beeFilter");
-    }
-  };
-
-  const fetchImage = async () => {
-    setAnimal(null);
-    setError("");
-    setImage(null);
-    setLoading(true);
-    setFilterClass(null);
-
-    try {
-      let response = await fetch(url);
-
-      if (!response.ok) {
-        setError(`Server error ${response.status} ${response.statusText}`); //
-      } else {
-        let data = await response.json();
-        let dataArray = data.hits; //getting to the arrays of data
-        let randomObject =
-          dataArray[Math.floor(Math.random() * dataArray.length)]; //randomly choose one object from the array
-        console.log(randomObject.webformatURL);
-
-        setImage(randomObject.webformatURL); //Storing url of API data into 'image' variable
-      }
-    } catch (error) {
-      setError("Error: " + error.message);
-    }
-
-    setLoading(false);
-  };
 
   const backToDefImg = () => {
-    setImage(null);
+    setAnimal(null);
+    let backToDefImg = animal;
+  };
+
+  const handleFilter = () => {
+    console.log(animal); //returning null for everything -- revisit this.
   };
 
   return (
@@ -85,7 +33,7 @@ export default function Action(props) {
       {/* Stack the columns on mobile by making one full-width and the other half-width */}
       <Container className="pb-5">
         <Row className="pb-5">
-          <Col className="mt-5" xs={12} md={12} lg={8}>
+          <Col className="mt-5">
             <div className="w-100 m-auto mt-5">
               {!image && error && <div class="errorDiv">Error: {error}</div>}
               {!image && loading && (
@@ -93,7 +41,7 @@ export default function Action(props) {
               )}
               {!image && !error && !loading && (
                 <div className="row">
-                  <div className="col-5 d-flex-justify-content-end">
+                  <div className="col-lg-5 col-12 d-flex-justify-content-end">
                     <h3>
                       Hi, I'm Pablita! <br />
                       I'm here to help
@@ -108,7 +56,6 @@ export default function Action(props) {
                       className="w-100 rounded float-center "
                       alt="Pablita, the jumping spider"
                       src={defaultImg}
-                      onClick={fetchImage}
                     />
                   </div>
                 </div>

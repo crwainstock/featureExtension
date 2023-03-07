0755153ca5119b1591644b8cef6feaf0d27303a0
Crystal <crwainstock@gmail.com>
2023-03-07 09:47:30 +0100

    navbar styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,36 +2,39 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function Header() {
   return (
     <header>
-      <Nav>
-        <NavLink
-          to="/"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
-        >
-          <Nav.Item>
-            <h5>Home</h5>
-          </Nav.Item>
-        </NavLink>
-        <NavLink
-          to="/look"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
-        >
-          <Nav.Item>
-            <h5>LOOK</h5>
-          </Nav.Item>
-        </NavLink>
-        <NavLink
-          to="/about"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
-        >
-          <Nav.Item>
-            <h5>About</h5>
-          </Nav.Item>
-        </NavLink>
-      </Nav>
+      <Navbar>
+        <Nav className="d-sm-justify-content-center">
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active-link" : null)}
+          >
+            <Nav.Item>
+              <h5>Home</h5>
+            </Nav.Item>
+          </NavLink>
+          <NavLink
+            to="/look"
+            className={({ isActive }) => (isActive ? "active-link" : null)}
+          >
+            <Nav.Item>
+              <h5>LOOK</h5>
+            </Nav.Item>
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "active-link" : null)}
+          >
+            <Nav.Item>
+              <h5>About</h5>
+            </Nav.Item>
+          </NavLink>
+        </Nav>
+      </Navbar>
     </header>
   );
 }

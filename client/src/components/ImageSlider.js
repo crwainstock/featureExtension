f874027ae84da44679bd5bedf1b10b023c785f40
Crystal <crwainstock@gmail.com>
2023-03-08 11:36:07 +0100

    image slider working! yay!

--- client/src/components/ImageSlider.js
+++ client/src/components/ImageSlider.js
@@ -1,10 +1,23 @@
 import React from "react";
-import "../components/imageSlider.css";
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from "react-compare-slider";
+
+import bee from "../images/animalVision/bee.png";
+import cat from "../images/animalVision/cat.png";
+import cow from "../images/animalVision/cow.png";
+import human from "../images/animalVision/human.png";
+import whale from "../images/animalVision/whale.png";
 
 export default function ImageSlider() {
   return (
     <div className="container">
       <h3>Image slider goes here</h3>
+      <ReactCompareSlider
+        itemOne={<ReactCompareSliderImage src={bee} alt="human" />}
+        itemTwo={<ReactCompareSliderImage src={human} alt="bee" />}
+      />
     </div>
   );
 }

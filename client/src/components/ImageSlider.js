e2205da3e81e0afc5259b6977e9669c64497773d
Crystal <crwainstock@gmail.com>
2023-03-08 09:21:19 +0100

    delete image slider attempt 1, going to try react npm package

--- client/src/components/ImageSlider.js
+++ client/src/components/ImageSlider.js
@@ -5,14 +5,6 @@ export default function ImageSlider() {
   return (
     <div className="container">
       <h3>Image slider goes here</h3>
-      <div className="img-comp-container">
-        <div className="img-comp-img">
-          <img src="./images/animalVision/human.png" width="300" height="200" />
-        </div>
-        <div className="img-comp-img img-comp-overlay">
-          <img src="./images/animalVision/bee.png" width="300" height="200" />
-        </div>
-      </div>
     </div>
   );
 }
